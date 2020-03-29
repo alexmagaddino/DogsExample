488f468a3a195d5b5722c0cb89fffafac01006f6
alexmagaddino <alex.magaddino@outlook.it>
2020-03-29 13:34:58 +0200

    setup layouts and Navigation

--- app/src/main/java/it/alexm/dogsexample/view/DetailFragment.kt
+++ app/src/main/java/it/alexm/dogsexample/view/DetailFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import it.alexm.dogsexample.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +21,13 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_to_list.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.back_to_list)
+        }
     }
 
 }
--- app/src/main/java/it/alexm/dogsexample/view/ListFragment.kt
+++ app/src/main/java/it/alexm/dogsexample/view/ListFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import it.alexm.dogsexample.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 
 /**
@@ -21,5 +23,13 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_show_detail.setOnClickListener {
+            val action = ListFragmentDirections.toDetail()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
