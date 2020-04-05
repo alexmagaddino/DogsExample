5daf765d3025637113e3854035babefcfd1bdac9
Magaddino Alessandro (Consulente) <alessandro.magaddino@consulenti.hype.it>
2020-04-05 11:09:09 +0200

    setup room, entity, dao and database

---
+++ app/src/main/java/it/alexm/dogsexample/model/DogDao.kt
@@ -0,0 +1,25 @@
+package it.alexm.dogsexample.model
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+
+/**
+ * Created by alexm on 05/04/2020
+ */
+@Dao
+interface DogDao {
+
+    @Insert
+    suspend fun insertAll(vararg dogs: DogBreed): List<Long>
+
+    @Query("SELECT * FROM DogBreed")
+    suspend fun getAllDogs(): List<DogBreed>
+
+    @Query("SELECT * FROM DOGBREED WHERE UUID = :uuid")
+    suspend fun getSelectedDog(uuid: Int) : DogBreed
+
+    @Query("DELETE FROM DOGBREED")
+    suspend fun clear()
+}
--- app/src/main/java/it/alexm/dogsexample/model/Model.kt
+++ app/src/main/java/it/alexm/dogsexample/model/Model.kt
@@ -1,24 +1,43 @@
 package it.alexm.dogsexample.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 
 /**
  * Created by alexm on 04/04/2020
  */
+@Entity
 data class DogBreed(
+    @ColumnInfo(name = "id")
     @SerializedName("id")
     val breedId: String?,
+
+    @ColumnInfo(name = "name")
     @SerializedName("name")
     val dogBreed: String?,
+
+    @ColumnInfo(name = "life_span")
     @SerializedName("life_span")
     val lifeSpan: String?,
+
+    @ColumnInfo(name = "breed_group")
     @SerializedName("breed_group")
     val breedGroup: String?,
+
+    @ColumnInfo(name = "bred_for")
     @SerializedName("bred_for")
     val bredFor: String?,
+
     @SerializedName("temperament")
     val temperament: String?,
+
+    @ColumnInfo(name = "url")
     @SerializedName("url")
     val imageUrl: String?
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+}
