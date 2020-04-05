b5de102029bae9d604f8dce213c5d2f23f7766ff
Magaddino Alessandro (Consulente) <alessandro.magaddino@consulenti.hype.it>
2020-04-05 11:35:53 +0200

    storing data and use of kotlin coroutines

--- app/src/main/java/it/alexm/dogsexample/viewmodel/ListViewModel.kt
+++ app/src/main/java/it/alexm/dogsexample/viewmodel/ListViewModel.kt
@@ -1,18 +1,21 @@
 package it.alexm.dogsexample.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import it.alexm.dogsexample.model.DogBreed
+import it.alexm.dogsexample.model.DogDatabase
 import it.alexm.dogsexample.model.api.DogApiService
+import kotlinx.coroutines.launch
 
 
 /**
  * Created by alexm on 04/04/2020
  */
-class ListViewModel : ViewModel() {
+class ListViewModel(application: Application) : BaseViewModel(application) {
 
     private val dogApiService = DogApiService()
     private val disposable = CompositeDisposable()
@@ -33,15 +36,31 @@ class ListViewModel : ViewModel() {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
-                    dogs.value = it
-                    error.value = false
-                    loading.value = false
+                    storeDogsLocally(it)
                 }, {
                     it.printStackTrace()
                     error.value = true
                     loading.value = false
                 })
         )
+    }
+
+    private fun updateLiveData(dogList: List<DogBreed>) {
+        dogs.value = dogList
+        error.value = false
+        loading.value = false
+    }
+
+    private fun storeDogsLocally(dogList: List<DogBreed>) {
+        launch {
+            val dao = DogDatabase(getApplication()).dogDao()
+            dao.clear()
+            val result = dao.insertAll(*dogList.toTypedArray())
+            updateLiveData(dogList.mapIndexed { i, dog ->
+                dog.uuid = result[i].toInt()
+                dog
+            })
+        }
     }
 
     override fun onCleared() {
